643f397b774eaea749a6cb1d3222eb5975822218
emman-henry <officialgr8henry@gmail.com>
2025-12-11 09:22:28 +0100

    Fix Vite config and deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        
+        cart: path.resolve(__dirname, 'src/cart/cart.html'),
+        product_list: path.resolve(__dirname, 'src/product_list/product_list.html'),
+        product_details: path.resolve(__dirname, 'src/product_details/product_details.html'),
+        wish_list: path.resolve(__dirname, 'src/wishlist/wish_list.html'),
       }
     }
   }
